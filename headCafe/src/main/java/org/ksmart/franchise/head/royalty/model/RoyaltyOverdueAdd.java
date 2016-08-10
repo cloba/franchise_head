e9c380596cdf7db43a8f9b749e7b1ae4046f2935
201-10 <201-10@201-10-PC>
2016-08-10 17:25:47 +0900

    [박종무/0810]

--- headCafe/src/main/java/org/ksmart/franchise/head/royalty/model/RoyaltyOverdueAdd.java
+++ headCafe/src/main/java/org/ksmart/franchise/head/royalty/model/RoyaltyOverdueAdd.java
@@ -1,5 +1,7 @@
 package org.ksmart.franchise.head.royalty.model;
 
+
+
 public class RoyaltyOverdueAdd {
 	
 	private String contractCode;
@@ -8,14 +10,10 @@ public class RoyaltyOverdueAdd {
 	private int headSellProfitBySubMonth; 
 	private String royaltyOverdueCode;
 	private String royaltyCode;
+	private int royaltyOverdueRest;
 
-	public String getRoyaltyPayActualDate() {
-		return royaltyPayActualDate;
-	}
-	public void setRoyaltyPayActualDate(String royaltyPayActualDate) {
-		this.royaltyPayActualDate = royaltyPayActualDate;
-	}
-	
+
+
 	public String getContractCode() {
 		return contractCode;
 	}
@@ -33,12 +31,14 @@ public class RoyaltyOverdueAdd {
 	}
 	public void setRoyaltyActualAmount(int royaltyActualAmount) {
 		this.royaltyActualAmount = royaltyActualAmount;
+		setRoyaltyOverdueRest();
 	}
 	public int getHeadSellProfitBySubMonth() {
 		return headSellProfitBySubMonth;
 	}
 	public void setHeadSellProfitBySubMonth(int headSellProfitBySubMonth) {
 		this.headSellProfitBySubMonth = headSellProfitBySubMonth;
+		setRoyaltyOverdueRest();
 	}
 	public String getRoyaltyCode() {
 		return royaltyCode;
@@ -47,6 +47,27 @@ public class RoyaltyOverdueAdd {
 		this.royaltyCode = royaltyCode;
 	}
 
+	//로얄티 연체 남은금액 구하는 메서드
+	public void setRoyaltyOverdueRest(){
+		int headSellProfitBySubMonth= this.headSellProfitBySubMonth;
+		int royaltyActualAmount = this.royaltyActualAmount;
+		int subtraction = headSellProfitBySubMonth - royaltyActualAmount;
+		this.royaltyOverdueRest = subtraction;
+
+	}
+	public int getRoyaltyOverdueRest() {
+		return royaltyOverdueRest;
+	}
+	public void setRoyaltyOverdueRest(int royaltyOverdueRest) {
+		this.royaltyOverdueRest = royaltyOverdueRest;
+	}
+	public String getRoyaltyPayActualDate() {
+		return royaltyPayActualDate;
+	}
+	public void setRoyaltyPayActualDate(String royaltyPayActualDate) {
+		this.royaltyPayActualDate = royaltyPayActualDate;
+	}
+	
  
 
 }
