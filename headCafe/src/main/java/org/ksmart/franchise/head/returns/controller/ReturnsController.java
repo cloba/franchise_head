616cc64a55495eb671c590e1573fb28f4d9c1dcd
soyoungahn <soyoungahn2706@gmail.com>
2016-08-19 15:15:05 +0900

    [안소영/0819] 반품상품 부트스트랩 적용 완료

--- headCafe/src/main/java/org/ksmart/franchise/head/returns/controller/ReturnsController.java
+++ headCafe/src/main/java/org/ksmart/franchise/head/returns/controller/ReturnsController.java
@@ -2,6 +2,7 @@ package org.ksmart.franchise.head.returns.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.ksmart.franchise.head.returns.model.Returns;
 import org.ksmart.franchise.head.returns.model.ReturnsProcess;
 import org.ksmart.franchise.head.returns.service.ReturnsService;
@@ -17,24 +18,23 @@ public class ReturnsController {
 
 	@Autowired
 	private ReturnsService returnsService;
+	Logger log = Logger.getLogger(this.getClass());
 
 	//반품 상품 리스트 조회 메서드
-	@RequestMapping(value="/viewReturnsList.do", method=RequestMethod.GET)
+	@RequestMapping(value="/viewReturnsList.do")
 	public String viewReturnsList(Model model, Returns returns, Search search) {
-		  System.out.println("ReturnsController의 viewReturnsList메서드 ");
-		  
-		  //반품 상품 리스트 조회 메서드
-		  List<Returns> returnsList = returnsService.viewReturnsListService(returns, search);
-		  
-		  model.addAttribute("returnsList", returnsList);
-
-	      return "/returns/viewReturnsList";
+		log.debug("ReturnsController의 viewReturnsList메서드 ");
+		//반품 상품 리스트 조회 메서드
+		List<Returns> returnsList = returnsService.viewReturnsListService(returns, search);
+		model.addAttribute("returnsList", returnsList);
+		
+		return "/returns/viewReturnsList";
     }
 	
 	//반품 상품  상세정보 조회 메서드
 	@RequestMapping(value="/viewReturnsDetail.do", method=RequestMethod.GET)
 	public String viewReturnsDetail(Model model, Returns returns) {
-		  System.out.println("ReturnsController의 viewReturnsDetail메서드 ");
+		log.debug("ReturnsController의 viewReturnsDetail메서드 ");
 		  returns =  returnsService.viewReturnsDetailService(returns);			 
 		  model.addAttribute("returns", returns);
 		  
@@ -44,8 +44,7 @@ public class ReturnsController {
 	//본사 반품 내용 확인 및 수락 메서드	
     @RequestMapping(value="/modifyReturnsApproval.do", method=RequestMethod.GET)
 	public String modifyReturnsApproval(Model model, ReturnsProcess returnsProcess){
-    	System.out.println("ReturnsController의 modifyReturnsApproval post메서드 ");
-    	
+    	log.debug("ReturnsController의 modifyReturnsApproval post메서드 ");
     	returnsService.addHeadDumpService(returnsProcess);    		
  
     	return "redirect:/viewReturnsList.do";
